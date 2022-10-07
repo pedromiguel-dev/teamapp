ed182a173dca455b675acc99c41e92e4c20e1efd
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2022-10-07 11:31:10 -0300

    logic for teams added :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   errorMessage = '';
   members: string[] = [];
   numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   onNumberOfTeamsInput(number: string) {
     this.numberOfTeams = Number(number);
@@ -25,5 +26,29 @@ export class AppComponent {
     this.errorMessage = '';
     this.members.push(this.newMemberName);
     this.newMemberName = '';
+  }
+  generateTeams(){
+    if (!this.numberOfTeams || this.numberOfTeams <= 0 ) {
+      this.errorMessage = "Set a number of teams";
+      return
+    }
+    const allMembers = [...this.members];
+    this.errorMessage = '';
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+        if(!member)break;
+        if(this.teams[i]){
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+
+      }
+    }
+    console.log(this.teams);
+
   }
 }
